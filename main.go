09439159db2176db3b6f179d7d2c25278e76017d
GrahamZiervogel <grahamziervogel@gmail.com>
2025-05-07 14:53:04 +0100

    Set up command registry

--- main.go
+++ main.go
@@ -1,33 +1,5 @@
 package main
 
-import (
-	"bufio"
-	"fmt"
-	"os"
-)
-
 func main() {
-	scanner := bufio.NewScanner(os.Stdin)
-
-	for {
-		fmt.Print("Pokedex > ")
-
-		if !scanner.Scan() {
-			if err := scanner.Err(); err != nil {
-				fmt.Fprintln(os.Stderr, "Error reading input:", err)
-			}
-			fmt.Println("\nExiting Pokedex REPL.")
-			break
-		}
-
-		userInput := scanner.Text()
-		cleanedWords := cleanInput(userInput)
-
-		if len(cleanedWords) == 0 {
-			continue
-		}
-
-		command := cleanedWords[0]
-		fmt.Println("Your command was:", command)
-	}
+	startRepl()
 }
--- repl.go
+++ repl.go
@@ -1,8 +1,47 @@
 package main
 
 import (
+	"bufio"
+	"fmt"
+	"os"
 	"strings"
 )
+
+func startRepl() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for {
+		fmt.Print("Pokedex > ")
+
+		if !scanner.Scan() {
+			if err := scanner.Err(); err != nil {
+				fmt.Fprintln(os.Stderr, "Error reading input:", err)
+			}
+			fmt.Println("\nExiting Pokedex REPL.")
+			break
+		}
+
+		userInput := scanner.Text()
+		cleanedWords := cleanInput(userInput)
+
+		if len(cleanedWords) == 0 {
+			continue
+		}
+
+		commandName := cleanedWords[0]
+		command, exists := getCommands()[commandName]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command")
+			continue
+		}
+	}
+}
 
 func cleanInput(text string) []string {
 	trimmedText := strings.TrimSpace(text)
@@ -11,4 +50,25 @@ func cleanInput(text string) []string {
 	words := strings.Fields(lowercasedText)
 
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
 }
